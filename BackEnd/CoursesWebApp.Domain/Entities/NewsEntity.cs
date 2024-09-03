3eaac06a361f9939400578a9653e6d5c5b19ddb7
VMekh8 <mekhvitaliy88@gmail.com>
2024-09-03 18:39:06 +0300

    Created NewsEntitity

---
+++ BackEnd/CoursesWebApp.Domain/Entities/NewsEntity.cs
@@ -0,0 +1,51 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace CoursesWebApp.Domain.Entities
+{
+    public class NewsEntity : Entity
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string ImageURL { get; set; }
+
+        private NewsEntity(string Title, string Description, string ImageURL)
+        {
+            this.Title = Title;
+            this.Description = Description;
+            this.ImageURL = ImageURL;
+        }
+
+        public static Result<NewsEntity> Create(string title, string desc, string url)
+        {
+            
+            if (string.IsNullOrWhiteSpace(title) || string.IsNullOrWhiteSpace(desc))
+                return Result.Failure<NewsEntity>("News title and description must be filled");
+
+            if (string.IsNullOrEmpty(url))
+                return Result.Failure<NewsEntity>("Image Url can`t be empty");
+
+            title.Trim();
+            desc.Trim();
+            url.Trim();
+
+            if (title.Length > 200)
+                return Result.Failure<NewsEntity>("Title can`t be longer, than 200 symbol`s");
+
+            if (desc.Length > 1000)
+                return Result.Failure<NewsEntity>("Description can`t be longer, than 1000 symbol`s");
+
+            if (Uri.IsWellFormedUriString(url, UriKind.Absolute))
+                return Result.Failure<NewsEntity>("Invalid URL format");
+
+            var news = new NewsEntity(
+                Title: title,
+                Description: desc,
+                ImageURL: url
+                );
+
+            return Result.Success(news);
+
+        }
+
+    }
+}
--- BackEnd/CoursesWebApp.Domain/obj/Debug/net8.0/CoursesWebApp.Domain.AssemblyInfo.cs
+++ BackEnd/CoursesWebApp.Domain/obj/Debug/net8.0/CoursesWebApp.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoursesWebApp.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2e9a2353cd36a5f3b50e8f9349517126b36b396")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b4c726b6440299f3b69491946a09ef83e5f3fce")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoursesWebApp.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoursesWebApp.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
