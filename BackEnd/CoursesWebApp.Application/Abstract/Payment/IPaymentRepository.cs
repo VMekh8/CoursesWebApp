6dac187c016e476f080e0fa5fd171c021b7845f0
VMekh8 <mekhvitaliy88@gmail.com>
2024-09-05 21:39:38 +0300

    Create Payment Repository

---
+++ BackEnd/CoursesWebApp.Application/Abstract/Payment/IPaymentRepository.cs
@@ -0,0 +1,9 @@
+﻿using CoursesWebApp.Application.Models;
+
+namespace CoursesWebApp.Application.Abstract.Payment
+{
+    public interface IPaymentRepository
+    {
+        Task<PaymentResultModel> CreatePaymentIntentAsync(decimal price);
+    }
+}
--- BackEnd/CoursesWebApp.Application/obj/Debug/net8.0/CoursesWebApp.Application.AssemblyInfo.cs
+++ BackEnd/CoursesWebApp.Application/obj/Debug/net8.0/CoursesWebApp.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoursesWebApp.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+782de01633795211f4b6d93cb51ebddc6935c383")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f25cd442a66acc24a973965737a41cd631b38bdd")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoursesWebApp.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoursesWebApp.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
