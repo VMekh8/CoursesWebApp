44909fb5fe318336d9244ba3bda0446170bb2a97
VMekh8 <mekhvitaliy88@gmail.com>
2024-09-14 14:32:12 +0300

    Create News Configuration

---
+++ BackEnd/CoursesWebApp.Infrastructure/Persistence/Configuration/NewsConfiguration.cs
@@ -0,0 +1,25 @@
+﻿
+using CoursesWebApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CoursesWebApp.Infrastructure.Persistence.Configuration
+{
+    public class NewsConfiguration : IEntityTypeConfiguration<NewsEntity>
+    {
+        public void Configure(EntityTypeBuilder<NewsEntity> builder)
+        {
+            builder.HasKey(e => e.Id);
+
+            builder.Property(e => e.Title)
+                .IsRequired();
+
+            builder.Property(e => e.Description)
+                .IsRequired();
+
+            builder.Property(e => e.ImageURL)
+                .IsRequired();
+
+        }
+    }
+}
