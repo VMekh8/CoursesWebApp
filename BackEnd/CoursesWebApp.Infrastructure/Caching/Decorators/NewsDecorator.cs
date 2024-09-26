72b9c67e83dc0ae0ff9e41943c8c031e1afbc163
VMekh8 <mekhvitaliy88@gmail.com>
2024-09-26 12:38:24 +0300

    Implement News Decorator

--- BackEnd/CoursesWebApp.Infrastructure/Caching/Decorators/NewsDecorator.cs
+++ BackEnd/CoursesWebApp.Infrastructure/Caching/Decorators/NewsDecorator.cs
@@ -1,11 +1,15 @@
-﻿using CoursesWebApp.Application.Abstract.CRUD;
+﻿using System.Diagnostics;
+using CoursesWebApp.Application.Abstract.CRUD;
 using CoursesWebApp.Domain.Entities;
 using CoursesWebApp.Infrastructure.Caching.Abstract;
+using StackExchange.Redis;
 
 namespace CoursesWebApp.Infrastructure.Caching.Decorators;
 
 public class NewsDecorator : IRepository<NewsEntity>, IReadOnlyRepository<NewsEntity>
 {
+    private const string AllNewsKey = "all_news";
+
     private readonly IRedisService<NewsEntity> _redisServices;
     private readonly IRepository<NewsEntity> _newsRepository;
     private readonly IReadOnlyRepository<NewsEntity> _newsReadOnlyRepository;
@@ -20,28 +24,87 @@ public class NewsDecorator : IRepository<NewsEntity>, IReadOnlyRepository<NewsEn
         _newsReadOnlyRepository = newsReadOnlyRepository;
     }
 
+    private async Task UpdateCacheAsync(NewsEntity entity)
+    {
+        try
+        {
+            await _redisServices.SetAsync(entity.Id, entity, TimeSpan.FromMinutes(5));
+
+            var cachedNewsList = await _redisServices.GetAllAsync(AllNewsKey);
+
+            cachedNewsList = cachedNewsList.Append(entity);
+
+            await _redisServices.SetListAsync(AllNewsKey, cachedNewsList, TimeSpan.FromMinutes(30));
+        }
+        catch (RedisException e)
+        {
+            Debug.WriteLine("Error with caching element");
+            Debug.WriteLine(e.Message);
+            throw;
+        }
+    }
+
     public async Task CreateAsync(NewsEntity entity)
     {
-        throw new NotImplementedException();
+        await _newsRepository.CreateAsync(entity);
+
+        await UpdateCacheAsync(entity);
     }
 
-    public Task UpdateAsync(NewsEntity entity)
+    public async Task UpdateAsync(NewsEntity entity)
     {
-        throw new NotImplementedException();
+        await _newsRepository.UpdateAsync(entity);
+
+        await UpdateCacheAsync(entity);
     }
 
-    public Task DeleteAsync(long id)
+    public async Task DeleteAsync(long id)
     {
-        throw new NotImplementedException();
+        await _newsRepository.DeleteAsync(id);
+
+        await _redisServices.RemoveAsync(id);
     }
 
-    public Task<IEnumerable<NewsEntity>> GetValuesAsync()
+    public async Task<IEnumerable<NewsEntity>> GetValuesAsync()
     {
-        throw new NotImplementedException();
+        var cachedNews = await _redisServices.GetAllAsync(AllNewsKey);
+
+        if (cachedNews is null)
+        {
+
+            var newsList = await _newsReadOnlyRepository.GetValuesAsync();
+
+            if (newsList is not null)
+            {
+                await _redisServices.SetListAsync(AllNewsKey, newsList, TimeSpan.FromMinutes(30));
+
+                return newsList;
+            }
+
+            return Enumerable.Empty<NewsEntity>();
+        }
+
+        return cachedNews;
     }
 
-    public Task<NewsEntity?> GetByIdAsync(long id)
+    public async Task<NewsEntity?> GetByIdAsync(long id)
     {
-        throw new NotImplementedException();
+        var cacheNews = await _redisServices.GetAsync(id);
+
+        if (cacheNews is null)
+        {
+            NewsEntity newsItem = await _newsReadOnlyRepository.GetByIdAsync(id);
+
+            if (newsItem is not null)
+            {
+                await _redisServices.SetAsync(id, newsItem, TimeSpan.FromMinutes(5));
+
+                return newsItem;
+            }
+
+            return null;
+        }
+
+        return cacheNews;
     }
 }
