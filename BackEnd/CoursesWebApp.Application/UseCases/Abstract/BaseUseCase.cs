42281ee5edf6d850a1b2ee509251b0e64f0b2799
VMekh8 <mekhvitaliy88@gmail.com>
2024-10-09 18:21:48 +0300

    Create rest of use cases for news. Update a Base use case class

--- BackEnd/CoursesWebApp.Application/UseCases/Abstract/BaseUseCase.cs
+++ BackEnd/CoursesWebApp.Application/UseCases/Abstract/BaseUseCase.cs
@@ -3,21 +3,24 @@
 public abstract class BaseUseCase<T> where T : class
 {
     
-    public virtual Task Create() => 
-        throw new NotImplementedException("Should be overrided");
+    public virtual Task Create(T entity) => 
+        throw new NotImplementedException("Should be overridden");
 
     public virtual Task CreateWithCourse(T entity, long courseId) =>
-        throw new NotImplementedException("Should be overrided");
+        throw new NotImplementedException("Should be overridden");
 
-    public virtual Task Update(T entity, long? courseId) => 
-        throw new NotImplementedException("Should be overrided");
+    public virtual Task Update(T entity) =>
+        throw new NotImplementedException("Should be overridden");
+
+    public virtual Task Update(T entity, long? courseId = null) => 
+        throw new NotImplementedException("Should be overridden");
 
     public virtual Task Remove(T entity) => 
-        throw new NotImplementedException("Should be overrided");
+        throw new NotImplementedException("Should be overridden");
 
     public virtual Task<IEnumerable<T>> Get() => 
-        throw new NotImplementedException("Should be overrided");
+        throw new NotImplementedException("Should be overridden");
 
     public virtual Task<T?> GetById(long id) => 
-        throw new NotImplementedException("Should be overrided");
+        throw new NotImplementedException("Should be overridden");
 }
---
+++ BackEnd/CoursesWebApp.Application/UseCases/NewsUseCases/UpdateNewsUseCase.cs
@@ -0,0 +1,21 @@
+﻿using CoursesWebApp.Application.Abstract.CRUD;
+using CoursesWebApp.Application.UseCases.Abstract;
+using CoursesWebApp.Domain.Entities;
+
+namespace CoursesWebApp.Application.UseCases.NewsUseCases;
+
+public class UpdateNewsUseCase : BaseUseCase<NewsEntity>
+{
+
+    private readonly IRepository<NewsEntity> _repository;
+
+    public UpdateNewsUseCase(IRepository<NewsEntity> repository)
+    {
+        _repository = repository;
+    }
+
+    public override async Task Update(NewsEntity entity)
+    {
+        await _repository.UpdateAsync(entity);
+    }
+}
