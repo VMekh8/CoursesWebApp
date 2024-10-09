258a21d185bf5f82ae53d8bef64e6c4b7c24eaa4
VMekh8 <mekhvitaliy88@gmail.com>
2024-10-09 17:46:59 +0300

    Create a BaseUseCase abstract class

--- BackEnd/CoursesWebApp.Application/UseCases/Abstract/BaseUseCase.cs
+++ BackEnd/CoursesWebApp.Application/UseCases/Abstract/BaseUseCase.cs
@@ -1,6 +1,17 @@
 ﻿namespace CoursesWebApp.Application.UseCases.Abstract;
 
-public class BaseUseCase
+public abstract class BaseUseCase<T> where T : class
 {
     
+    public virtual Task Create() => Task.CompletedTask;
+
+    public virtual Task CreateWithCourse(T entity, long courseId) => Task.CompletedTask;
+
+    public virtual Task Update(T entity, long? courseId) => Task.CompletedTask;
+
+    public virtual Task Remove(T entity) => Task.CompletedTask;
+
+    public virtual Task Get() => Task.CompletedTask;
+
+    public virtual Task GetById(long id) => Task.CompletedTask;
 }
--- BackEnd/CoursesWebApp.Domain/obj/Debug/net8.0/CoursesWebApp.Domain.AssemblyInfo.cs
+++ BackEnd/CoursesWebApp.Domain/obj/Debug/net8.0/CoursesWebApp.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoursesWebApp.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdd134ff0b24865a3eb3becfe5440eb579dac847")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20791b6638ece99db0f1dcaeca8650338d613e15")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoursesWebApp.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoursesWebApp.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
