6e7f7f05f80aaa440e17c6b07dbe672ef387de0a
VMekh8 <mekhvitaliy88@gmail.com>
2024-10-09 23:59:09 +0300

    Create DI for Students, Courses, Lessons Use Cases

---
+++ BackEnd/CoursesWebApp.Application/UseCases/LessonUseCase/CreateLessonUseCase.cs
@@ -0,0 +1,21 @@
+﻿using CoursesWebApp.Application.Abstract.CRUD;
+using CoursesWebApp.Application.UseCases.Abstract;
+using CoursesWebApp.Domain.Entities;
+
+namespace CoursesWebApp.Application.UseCases.LessonUseCase;
+
+public class CreateLessonUseCase : LessonBaseUseCases
+{
+
+    private readonly IRepository<LessonEntity> _repository;
+
+    public CreateLessonUseCase(IRepository<LessonEntity> repository)
+    {
+        _repository = repository;
+    }
+
+    public override async Task Create(LessonEntity entity, long CourseId)
+    {
+
+    }
+}
