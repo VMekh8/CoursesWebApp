1f3acc836363a4c79dbce6847157b6f724b06e1a
VMekh8 <mekhvitaliy88@gmail.com>
2024-10-26 22:18:19 +0300

    Create ViewModels for Students contracts
    - Add Student Responce record
    - Add Student Request record

---
+++ BackEnd/CoursesWebApp/DTO/Request/StudentDTO/StudentRequest.cs
@@ -0,0 +1,11 @@
+﻿using CoursesWebApp.Domain.Entities;
+
+namespace CoursesWebApp.API.DTO.Request.StudentDTO;
+
+public record StudentRequest(
+    string Firsname,
+    string Lastname,
+    decimal Age,
+    string? ImageURl,
+    List<EnrollmentEntity> Enrollments 
+    );
---
+++ BackEnd/CoursesWebApp/DTO/Responce/StudentDTO/StudentResponce.cs
@@ -0,0 +1,10 @@
+﻿namespace CoursesWebApp.API.DTO.Responce.StudentDTO;
+
+public record StudentResponce(
+    long StudentId,
+    string Firsname,
+    string Lastname, 
+    decimal Age,
+    string? ImageURl,
+    List<long> EnrollmentsId
+    );
